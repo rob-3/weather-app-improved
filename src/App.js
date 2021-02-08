5ce4c9b98c172af8c64c8b38c78dbe724f7d3712
Robert Boyd III <rob@robertcboyd.com>
2021-02-07 22:21:02 -0500

    Add search button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'fontsource-roboto';
-import { Container, TextField, Box } from "@material-ui/core";
+import { Container, TextField, Box, Button } from "@material-ui/core";
 import { useState } from "react";
 
 function App() {
@@ -8,11 +8,18 @@ function App() {
 
   const typeHandler = event => setQuery(event.target.value);
 
+  const fetchCityData = () => {
+    console.log("Fetching data...");
+  };
+
   return (
     <Container>
       <Box m={5}>
         <form>
           <TextField label="Type a city!" value={query} onChange={typeHandler}/>
+          <Button variant="contained" color="primary" onClick={fetchCityData}>
+            Get Weather Data
+          </Button>
         </form>
       </Box>
     </Container>
