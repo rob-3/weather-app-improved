d60965f3c75672a143a96fb245afea83310fb66b
Robert Boyd III <rob@robertcboyd.com>
2021-02-07 22:18:15 -0500

    Add input box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'fontsource-roboto';
+import { Container, TextField, Box } from "@material-ui/core";
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  const typeHandler = event => setQuery(event.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Box m={5}>
+        <form>
+          <TextField label="Type a city!" value={query} onChange={typeHandler}/>
+        </form>
+      </Box>
+    </Container>
   );
 }
 
